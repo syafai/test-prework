33b48f1d569f9148d1807322027f207c40d946ab
Saraa Yafai <yafaisaraa@gmail.com>
2023-05-22 14:58:50 -0400

    initial commit

---
+++ prework.py
@@ -0,0 +1,22 @@
+
+
+def hello_name(user_name):
+    print("Hello" + user_name.upper() + "!")
+    
+hello_name('Carlos')
+
+def odd_numbers():
+    for i in range(1,101,2):
+        print(i)
+        
+#odd_numbers()
+        
+def odd_numbers():
+    numbers = list(range(0, 101))
+for number in numbers:
+        if number % 2 != 0:
+    	    print(number)
+        
+odd_numbers2()
+
+
